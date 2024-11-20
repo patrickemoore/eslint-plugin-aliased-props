38fa62f6458b737f836810f28966741d7d0a00cf
Patrick Moore <patrick@endla.com>
2024-11-20 14:40:55 +1100

    add support for react.memo

--- tests/lib/rules/require-aliased-props.test.js
+++ tests/lib/rules/require-aliased-props.test.js
@@ -24,6 +24,8 @@ const flatConfig = [
 
 const ruleTester = new RuleTester(flatConfig[0]);
 
+const ERROR = [{ message: 'Props should use a named interface or type alias instead of an inline type.' }];
+
 const invalidCodeSamples = [
   {
     code: `
@@ -35,7 +37,7 @@ const invalidCodeSamples = [
       }
     `,
     filename: 'test.tsx',
-    errors: [{ message: 'Props should use a named interface or type alias instead of an inline type.' }],
+    errors: ERROR,
   },
   {
     code: `
@@ -44,7 +46,7 @@ const invalidCodeSamples = [
       };
     `,
     filename: 'test.tsx',
-    errors: [{ message: 'Props should use a named interface or type alias instead of an inline type.' }],
+    errors: ERROR,
   },
   {
     code: `
@@ -53,8 +55,38 @@ const invalidCodeSamples = [
       }
     `,
     filename: 'test.tsx',
-    errors: [{ message: 'Props should use a named interface or type alias instead of an inline type.' }],
+    errors: ERROR,
   },
+  {
+    code: `
+      const SphereMarker = React.memo((props: {
+        position?: Vector3;
+        color?: string;
+      }) => {
+        return <div />;
+      });
+    `,
+    filename: 'test.tsx',
+    errors: ERROR,
+  },
+  {
+    code: `
+      const MyComponent = React.memo((props: { name: string, age: number }) => {
+        return <div>{props.name}</div>;
+      });
+    `,
+    filename: 'test.tsx',
+    errors: ERROR,
+  },
+  {
+    code: `
+      const AnotherComponent = React.memo((props: { title: string }) => {
+        return <h1>{props.title}</h1>;
+      });
+    `,
+    filename: 'test.tsx',
+    errors: ERROR,
+  }
 ];
 
 const validCodeSamples = [
@@ -93,6 +125,82 @@ const validCodeSamples = [
       function AnotherComponent(props: AnotherComponentProps) {
         return <h1>{props.title}</h1>;
       }
+    `,
+    filename: 'test.tsx',
+  },
+  {
+    code: `
+      interface SphereMarkerProps {
+        position?: Vector3;
+        color?: string;
+      }
+
+      const SphereMarker = React.memo((props: SphereMarkerProps) => {
+        return <div />;
+      });
+    `,
+    filename: 'test.tsx',
+  },
+  {
+    code: `
+      type MyComponentProps = {
+        name: string,
+        age: number,
+      };
+
+      const MyComponent = React.memo((props: MyComponentProps) => {
+        return <div>{props.name}</div>;
+      });
+    `,
+    filename: 'test.tsx',
+  },
+  {
+    code: `
+      interface AnotherComponentProps {
+        title: string;
+      }
+
+      const AnotherComponent = React.memo((props: AnotherComponentProps) => {
+        return <h1>{props.title}</h1>;
+      });
+    `,
+    filename: 'test.tsx',
+  },
+  {
+    code: `
+      interface SphereMarkerProps {
+        position?: Vector3;
+        color?: string;
+      }
+
+      const SphereMarker = React.memo(function SphereMarker(props: SphereMarkerProps) {
+        return <div />;
+      });
+    `,
+    filename: 'test.tsx',
+  },
+  {
+    code: `
+      type MyComponentProps = {
+        name: string,
+        age: number,
+      };
+
+      const MyComponent = React.memo(function MyComponent(props: MyComponentProps) {
+        return <div>{props.name}</div>;
+      });
+    `,
+    filename: 'test.tsx',
+  },
+  {
+    code: `
+      interface AnotherComponentProps {
+        title: string;
+      }
+
+      const AnotherComponent = React.memo(function AnotherComponent(props: AnotherComponentProps) {
+        return <h1>{props.title}</h1>;
+      });
     `,
     filename: 'test.tsx',
   },
